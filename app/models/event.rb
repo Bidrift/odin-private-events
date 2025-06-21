43bd3b3eb6bc4ff0451a714946b2d0f982d110bd
Bidrift <yassine.fighter@gmail.com>
2025-06-22 00:29:49 +0100

    Add useful methods in event

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,6 +7,18 @@ class Event < ApplicationRecord
     validates :location, presence: true, length: { minimum: 5, maximum: 200 }
     validates :creator, presence: true
 
+    scope :in_past, ->  {
+        where(time: ...DateTime.now)
+    }
+
+    scope :in_future, ->  {
+        where(time: DateTime.now...)
+    }
+
     has_many :attendances, foreign_key: 'attended_event_id'
     has_many :attendees, through: :attendances
+
+    def is_in_future?
+        time.present? && time > DateTime.now
+    end
 end
