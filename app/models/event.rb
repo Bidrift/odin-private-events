63f918cde28eb8addb71eab84c1d1ba8f92f2957
Bidrift <yassine.fighter@gmail.com>
2025-06-21 11:20:13 +0100

    Add event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,11 @@
 class Event < ApplicationRecord
+    belongs_to :creator, class_name: 'User'
+
+    validates :name , presence: true, length: { minimum: 3, maximum: 100 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 500 }
+    validates :date, presence: true
+    validates :location, presence: true, length: { minimum: 5, maximum: 200 }
+    validates :creator, presence: true
+    
+    
 end
