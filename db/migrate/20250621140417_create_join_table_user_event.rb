6a594e62796e4baa3bdb9cca36bf68c27567d375
Bidrift <yassine.fighter@gmail.com>
2025-06-21 15:29:48 +0100

    Add attendance model and migrate

---
+++ db/migrate/20250621140417_create_join_table_user_event.rb
@@ -0,0 +1,10 @@
+class CreateJoinTableUserEvent < ActiveRecord::Migration[8.0]
+  def change
+    create_table :attendances do |t|
+      t.timestamps
+    end
+    add_reference :attendances, :attendee, null: false, foreign_key: { to_table: :users }
+    add_reference :attendances, :attended_event, null: false, foreign_key: { to_table: :events }
+    add_index :attendances, [:attendee_id, :attended_event_id], unique: true
+  end
+end
