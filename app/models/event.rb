9902a6d46f1b7e666fe05f2edd572fc1201cac03
Bidrift <yassine.fighter@gmail.com>
2025-06-21 11:31:37 +0100

    Change event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,9 +3,8 @@ class Event < ApplicationRecord
 
     validates :name , presence: true, length: { minimum: 3, maximum: 100 }
     validates :description, presence: true, length: { minimum: 10, maximum: 500 }
-    validates :date, presence: true
+    validates :time, comparison: { greater_than: DateTime.now}
     validates :location, presence: true, length: { minimum: 5, maximum: 200 }
     validates :creator, presence: true
-    
     
 end
